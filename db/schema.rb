165842bc8bb2170a20c380071c228a1efe067c00
aboelleef <ahmedaboeleef@trycarriage.com>
2019-04-01 00:49:21 +0300

    use pg instead of sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_03_28_211403) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "chapters", force: :cascade do |t|
     t.string "name"
     t.text "desc"

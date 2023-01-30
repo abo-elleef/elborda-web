b653aae17517d285e8479009421af95ec9f57dcb
abo-elleef <a.aboelllef@gmail.com>
2023-01-30 23:07:20 +0200

    upgrade to rails 6

--- db/schema.rb
+++ db/schema.rb
@@ -2,23 +2,26 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_19_231016) do
+ActiveRecord::Schema.define(version: 2023_01_30_210313) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
     t.string "resource_type"
-    t.integer "resource_id"
+    t.bigint "resource_id"
     t.string "author_type"
-    t.integer "author_id"
+    t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
@@ -38,12 +41,29 @@ ActiveRecord::Schema.define(version: 2020_04_19_231016) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
+  create_table "articles", force: :cascade do |t|
+    t.string "name"
+    t.string "desc"
+    t.integer "repeats", default: 1
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "articles_tags", force: :cascade do |t|
+    t.integer "tag_id"
+    t.integer "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "chapters", force: :cascade do |t|
     t.string "name"
     t.text "desc"
     t.integer "poem_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "video_id"
+    t.text "content"
   end
 
   create_table "lines", force: :cascade do |t|
@@ -72,11 +92,28 @@ ActiveRecord::Schema.define(version: 2020_04_19_231016) do
     t.datetime "updated_at", null: false
     t.integer "chapter_size"
     t.boolean "published", default: false
+    t.string "slug"
+    t.index ["slug"], name: "index_poems_on_slug", unique: true
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
+  create_table "section_lines", force: :cascade do |t|
+    t.text "body"
+    t.integer "repeats", default: 1
+    t.integer "section_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "sort", default: 1
+  end
+
+  create_table "sections", force: :cascade do |t|
+    t.integer "repeats", default: 1
+    t.integer "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "sort", default: 1
+  end
+
+  create_table "tags", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

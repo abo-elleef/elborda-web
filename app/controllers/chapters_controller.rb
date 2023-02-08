6a7c3e539cfcab7ae1ec2f0e36aa75ca5cb31980
abo-elleef <a.aboelllef@gmail.com>
2023-02-09 00:29:10 +0200

    Add-related-poems-chapters

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,19 +1,22 @@
 class ChaptersController < ApplicationController
-
+  
   def show
     @chapter = Chapter.find(params[:id])
     @poem = @chapter.poem
+    set_related_poems
   end
 
   def next
     @chapter = Chapter.where(" id >  #{params[:id]}").first || Chapter.last
     @poem = @chapter.poem
+    set_related_poems
     render :show
   end
 
   def previous
     @chapter = Chapter.where(" id <  #{params[:id]}").last || Chapter.first
     @poem = @chapter.poem
+    set_related_poems
     render :show
   end
 

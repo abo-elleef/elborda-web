54e671d69389e9d4808b9d9a7fc2c938620d1738
abo-elleef <a.aboelllef@gmail.com>
2023-02-27 07:48:18 +0200

    remove dark mode

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,7 +8,7 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.paths += ['*.js', '*.scss']
-Rails.application.config.assets.precompile += %w( dark_mode.css )
+# Rails.application.config.assets.precompile += %w( dark_mode.css )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets

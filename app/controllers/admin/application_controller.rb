9bd0adcc36bfe66d52ec89706b841fff11e2b2f0
abo-elleef <a.aboelllef@gmail.com>
2024-03-25 23:55:33 +0200

    add authentication for admin panel

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -6,6 +6,7 @@
 # you're free to overwrite the RESTful controller actions.
 module Admin
   class ApplicationController < Administrate::ApplicationController
+    http_basic_authenticate_with name: "administrator", password: "@dm!n!str@t0r"
     before_action :authenticate_admin
 
     def authenticate_admin

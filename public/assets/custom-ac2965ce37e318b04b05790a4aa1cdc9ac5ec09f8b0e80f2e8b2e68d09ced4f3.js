4f8f31886c6f4ef9eecc20a20a1a17ff625ab479
abo-elleef <a.aboelllef@gmail.com>
2025-05-25 12:07:31 +0300

    compile assets

---
+++ public/assets/custom-ac2965ce37e318b04b05790a4aa1cdc9ac5ec09f8b0e80f2e8b2e68d09ced4f3.js
@@ -0,0 +1,8 @@
+// var delayTimer;
+// $(window).ready(function () {
+//     $(".line-container").click(function(){
+//         $(this).parent().find("#sharing_list").toggleClass("d-block").toggleClass("d-none");
+//     })
+// });
+//
+//;
